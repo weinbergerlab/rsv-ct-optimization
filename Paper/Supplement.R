d822722cb34f119de5279f57de59bf57f4c0a90f
Ben Artin <ben@artins.org>
2019-03-01 00:11:09 -0500

    Migrating supplement plots into paper text

--- Paper/Supplement.R
+++ Paper/Supplement.R
@@ -1,20 +1,10 @@
+# ---- supplement ----
+library(ggstance)
+
 tollandObs = obsByCounty %>% filter(county=="Tolland")
 tollandModel = gam(rsv ~ s(time, k=20, bs="cp", m=3), family=poisson, data=tollandObs)
 
-ggplot(tollandObs) +
-    theme_light(base_size=plotTextBaseSize) +
-    geom_point(aes(x=time, y=rsv.cum)) +
-    scale_x_continuous(breaks=monthBoundaries$mid, labels=epiWeekLabels, limits=range(c(monthBoundaries$min, monthBoundaries$max)), expand=c(0, 0)) + 
-    theme(
-        panel.grid.major.x=element_blank(),
-        legend.title=element_blank(),
-        axis.title.y=element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.text.x=element_text(angle=90, hjust=0.5, vjust=0.5),
-        legend.position="bottom"
-    )
-
-tollandNsim = 5
+tollandNsimMini = 5
 
 randomMVN = function(mu, sig, nsim) {
   L = mroot(sig)
@@ -22,9 +12,9 @@ randomMVN = function(mu, sig, nsim) {
   t(mu + L %*% matrix(rnorm(m * nsim), m, nsim))
 }
 
-tollandParams = randomMVN(coef(tollandModel), tollandModel$Vp, tollandNsim)
+tollandParamsMini = randomMVN(coef(tollandModel), tollandModel$Vp, tollandNsimMini)
 
-tollandPred = tollandParams %>%
+tollandPredMini = tollandParamsMini %>%
   apply(1, function(params) { outbreak.calc.cum(1)(tollandModel, params, modelTime) } ) %>%
   data.frame() %>%
   cbind(time=modelTime) %>%
@@ -32,50 +22,18 @@ tollandPred = tollandParams %>%
   mutate(variable=as.numeric(substr(variable, 2, 2))) %>%
   rename(sim=variable, rsv.cum.frac=value)
 
-ggplot(tollandPred) +
-  theme_light(base_size=plotTextBaseSize) +
-  geom_point(data=tollandObs, aes(x=time, y=rsv.cum.frac)) +
-  geom_line(aes(x=time, y=rsv.cum.frac, group=sim)) +
-  geom_segment(aes(x=-Inf, y=seasonThreshold, xend=+Inf, yend=seasonThreshold), linetype="11", data=data.frame(), size=.375) + 
-  scale_x_continuous(breaks=monthBoundaries$mid, labels=epiWeekLabels, limits=range(c(monthBoundaries$min, monthBoundaries$max)), expand=c(0, 0)) + 
-  theme(
-    panel.grid.major.x=element_blank(),
-    legend.title=element_blank(),
-    axis.title.y=element_blank(),
-    axis.ticks.x=element_blank(),
-    axis.text.x=element_text(angle=90, hjust=0.5, vjust=0.5),
-    legend.position="bottom"
-  )
-
-tollandOnset = tollandParams %>%
+tollandOnsetMini = tollandParamsMini %>%
   apply(1, function(params) { outbreak.calc.thresholds(seasonThreshold, 1-seasonThreshold)(tollandModel, params, modelTime) } ) %>%
   bind_rows() %>%
   select(onset)
 
-zoomedStartWeek = min(monthBoundaries$min) + 11
-zoomedEndWeek = zoomedStartWeek + 13
+zoomedStartWeek = min(monthBoundaries$min) + 5
+zoomedEndWeek = zoomedStartWeek + 21
 
-ggplot(tollandPred) +
-  theme_light(base_size=plotTextBaseSize) +
-  geom_line(aes(x=time, y=rsv.cum.frac, group=sim), color="gray") +
-  geom_segment(aes(x=-Inf, y=seasonThreshold, xend=+Inf, yend=seasonThreshold), linetype="11", data=data.frame(), size=.375) + 
-  scale_y_continuous(limits=c(0, 2*seasonThreshold)) + 
-  scale_x_continuous(breaks=monthBoundaries$mid, labels=epiWeekLabels, limits=c(zoomedStartWeek, zoomedEndWeek), expand=c(0, 0)) + 
-  geom_point(data=tollandOnset, aes(x=onset, y=seasonThreshold)) + 
-  geom_segment(data=tollandOnset, aes(x=onset, xend=onset, y=seasonThreshold, yend=0), linetype="11") + 
-  theme(
-    panel.grid.major.x=element_blank(),
-    legend.title=element_blank(),
-    axis.title.y=element_blank(),
-    axis.ticks.x=element_blank(),
-    axis.text.x=element_text(angle=90, hjust=0.5, vjust=0.5),
-    legend.position="bottom"
-  )
+tollandNsim = simulations
+tollandParamsFull = randomMVN(coef(tollandModel), tollandModel$Vp, tollandNsimFull)
 
-tollandNsimMany = 5000
-tollandParamsMany = randomMVN(coef(tollandModel), tollandModel$Vp, tollandNsimMany)
-
-tollandPredMany = tollandParamsMany %>%
+tollandPredFull = tollandParamsFull %>%
   apply(1, function(params) { outbreak.calc.cum(1)(tollandModel, params, modelTime) } ) %>%
   data.frame() %>%
   cbind(time=modelTime) %>%
@@ -88,32 +46,11 @@ tollandPredMany = tollandParamsMany %>%
     max=max(rsv.cum.frac)
   )
 
-tollandOnsetMany = tollandParamsMany %>%
+tollandOnsetFull = tollandParamsFull %>%
   apply(1, function(params) { outbreak.calc.thresholds(seasonThreshold, 1-seasonThreshold)(tollandModel, params, modelTime) } ) %>%
   bind_rows() %>%
   select(onset)
 
-ggplot(tollandPredMany) +
-  theme_light(base_size=plotTextBaseSize) +
-  geom_ribbon(aes(x=time, ymin=min, ymax=max), fill="gray", na.rm=FALSE) +
-  geom_segment(aes(x=-Inf, y=seasonThreshold, xend=+Inf, yend=seasonThreshold), linetype="11", data=data.frame(), size=.375) + 
-  # geom_jitter(data=tollandOnsetMany, aes(y=onset, x=seasonThreshold), width=0.005) + 
-  geom_violinh(
-    data=tollandOnsetMany,
-    aes(x=onset, y=seasonThreshold),
-    width=.01,
-    fill="gray", color="black",
-    trim=FALSE, draw_quantiles=c(0.025, 0.5, 0.975)
-  ) +
-  scale_x_continuous(breaks=monthBoundaries$mid, labels=epiWeekLabels, expand=c(0, 0)) +
-  scale_y_continuous() +
-  coord_cartesian(xlim=c(zoomedStartWeek, zoomedEndWeek), ylim=c(0, 4*seasonThreshold)) +
-  theme(
-    panel.grid.major.x=element_blank(),
-    legend.title=element_blank(),
-    axis.title.y=element_blank(),
-    axis.ticks.x=element_blank(),
-    axis.text.x=element_text(angle=90, hjust=0.5, vjust=0.5),
-    legend.position="bottom"
-  )
+zoomedStartWeekFull = min(monthBoundaries$min) + 5
+zoomedEndWeekFull = zoomedStartWeekFull + 21
 
